ee117360c8e5c8f362505f42302512a409252de1
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2020-11-24 16:50:36 -0600

    merge

--- webapi/app.js
+++ webapi/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const { createConnection } = require('typeorm');
 const app = express();
 const port = 3000;
+const express = require('express')
+const fileUpload = require('express-fileupload');
+const app = express()
+const port = 3000
+
+app.use(fileUpload({
+  limits: { fileSize: 1 * 1024 * 1024 },
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello Weclaraline!')
@@ -25,3 +33,25 @@ createConnection({
     console.log(`Example app listening at http://localhost:${port}`)
   });
 }).catch(error => console.log(error));
+
+app.post('/upload', function(req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.xml;
+  var str = sampleFile.data.toString('ascii')
+  // // Use the mv() method to place the file somewhere on your server
+  // sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
+  //   if (err)
+  //     return res.status(500).send(err);
+
+  //   res.send('File uploaded!');
+  // });
+  res.send(str)
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
