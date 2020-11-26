3f27fa56efa61f39bab9a2b5ea8ac51a8dfc576f
Omar Antonio Madriz Almanza <omar.madriz@OmarMadrizs-MacBook-Pro.local>
2020-11-26 16:55:08 -0600

    Post and Get enpoints for FAQ

--- webapi/routes/faq.router.js
+++ webapi/routes/faq.router.js
@@ -2,10 +2,13 @@ const route = require('express').Router();
 const FAQService = require('../src/services/FAQ/FAQService');
 
 route.post('/', (req, res) => {
-    console.log(req.body);
-    result = FAQService.addQuestionAndAnswer(req.question, req.answer);
-    res.status(200).send({message : result})
+    FAQService.addQuestionAndAnswer(req.body.question, req.body.answer)
+        .then(result => res.status(200).send({message : result}));
 });
 
+route.get('/', (req, res) => {
+    FAQService.getQuestionsAndAnswers()
+        .then(result => res.send(result));
+});
 
 module.exports = route;
--- webapi/src/services/FAQ/FAQService.js
+++ webapi/src/services/FAQ/FAQService.js
@@ -4,12 +4,15 @@ const { getManager } = require("typeorm");
 
 async function addQuestionAndAnswer(question, answer) {
     let new_faq = new FAQ_model(question, answer);
+    return await getManager().getRepository(FAQ_entity).save(new_faq);
+}
 
-    const response = await getManager().getRepository(FAQ_entity).save(new_faq);
-    return response;
+async function getQuestionsAndAnswers() {
+    return await getManager().getRepository(FAQ_entity).find();;
 }
 
 
 module.exports = {
-    addQuestionAndAnswer
+    addQuestionAndAnswer,
+    getQuestionsAndAnswers
 }
