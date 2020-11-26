d2c34d664fa6f835aa23a74fdc24a70276afed85
osvaldoec <73859327+osvaldoec@users.noreply.github.com>
2020-11-26 14:22:02 -0600

    endpoints (get) recommendations/ and recomendations/:key

--- webapi/src/services/recomendation/RecomendationService.js
+++ webapi/src/services/recomendation/RecomendationService.js
@@ -1,10 +1,46 @@
-function get(filterKey) {
+const { getManager, getRepository } = require("typeorm");
+const { groupBy, flatten } = require('ramda');
+const recommendationEntity = require("../../entity/RecomendationSchema");
+const { Recomendation } = require("../../model/Recomendation");
+
+const groupByKey = groupBy(recommendations => `${recommendations.key}`);
+
+const buildRecommendation = recommendationsData => {
+  const recommGrouped = groupByKey(recommendationsData);
+
+  return Object.keys(recommGrouped).map(recommKey => {
+    const recommArray = [];
+    recommGrouped[recommKey].forEach(recommendation => recommArray.push({ "description": recommendation.text }));
+    return ({
+      "key": recommKey,
+      "deduction": recommGrouped[recommKey][0].concept,
+      "recommendations": recommArray
+    });
+  });
+};
+
+async function get(filterKey) {
   if (filterKey) {
-    return "some recomendations" + filterKey;
+    const recommendationsData = await getManager().getRepository(recommendationEntity).find({
+      where: {
+        key: filterKey
+      }
+    });
+
+    return buildRecommendation(recommendationsData);
   }
-  return "some recomendations";
+
+  const recommendationsData = await getManager().getRepository(recommendationEntity).find();
+  return buildRecommendation(recommendationsData);
+}
+
+async function post({ key = '', concept = '', text = '' }) {
+  const recommendation = new Recomendation(key, concept, text);
+  const insertResult = await getManager().getRepository(recommendationEntity).save(recommendation);
+  return insertResult;
 }
 
 module.exports = {
-  get: get,
+  get,
+  post,
 };
