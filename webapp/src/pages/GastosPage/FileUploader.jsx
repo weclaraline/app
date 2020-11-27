5084e4370ad56603dffc787da194290fc61d79c7
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2020-11-27 15:13:16 -0600

    modal component expenses

--- webapp/src/pages/GastosPage/FileUploader.jsx
+++ webapp/src/pages/GastosPage/FileUploader.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ServiceAPI from '../../api/ServiceAPI';
-
-import {getCurrentLoggedUserInfo} from '../../utils/LogIn'
+import ModalGastos from './ModalGastos';
+import { getCurrentLoggedUserInfo } from '../../utils/LogIn';
 
 import {
   Paper,
@@ -24,56 +24,67 @@ const useStyles = makeStyles(() =>
 
 const FileUploader = () => {
   const classes = useStyles();
-  const [fileUUID, setFileUUID] = useState();
-  const [concept, setConcept] = useState("");
-  const [description, setDescription] = useState("");
+  const [fileUUID, setFileUUID] = useState('');
+  const [concept, setConcept] = useState('');
+  const [description, setDescription] = useState('');
   const [total, setTotal] = useState(0);
+  const [openModal, setOpenModal] = React.useState(false);
+
   const api = new ServiceAPI();
 
-
   async function uploadFile(file_upload) {
-    const data = new FormData();   
-    let userId   = await getCurrentLoggedUserInfo();
+    const data = new FormData();
+    let userId = await getCurrentLoggedUserInfo();
     data.append('xml', file_upload);
     data.append('uid', userId.googleId);
 
-  api.createRequest().post('invoices/upload',data, {
-    })
-    .then((res) => {
-      setFileUUID(res.data.uuid);
-      setConcept(res.data.concept);
-      setDescription(res.data.description);
-      setTotal(res.data.total);
-    });
+    api
+      .createRequest()
+      .post('invoices/upload', data, {})
+      .then((res) => {
+        setFileUUID(res.data.uuid);
+        setConcept(res.data.concept);
+        setDescription(res.data.description);
+        setTotal(res.data.total);
+        setOpenModal(true);
+      });
   }
 
   return (
     <Paper className={classes.container}>
-
+      {
+        <ModalGastos
+          fileUUID={fileUUID}
+          concept={concept}
+          description={description}
+          total={total}
+          openModal={openModal}
+        ></ModalGastos>
+      }
       <Container>
         <Grid container spacing={4}>
-            <Grid item md={3}>
-              <Typography variant="h5" gutterBottom>
-                Facturas
-              </Typography>
-            </Grid>
+          <Grid item md={3}>
+            <Typography variant="h5" gutterBottom>
+              Facturas
+            </Typography>
+          </Grid>
         </Grid>
       </Container>
       <Container>
         <Grid container spacing={4}>
-        <Grid item md={4}>
+          <Grid item md={4}>
             <Button variant="contained" component="label" color="primary">
               SELECCIONAR FACTURA
               <input
                 type="file"
                 hidden
-                onChange={(event) => uploadFile(event.target.files[0])}                
+                onChange={(event) => uploadFile(event.target.files[0])}
               />
             </Button>
-            </Grid>
+          </Grid>
         </Grid>
         <p>
-            {fileUUID} {concept} {description} {total}
+          {fileUUID} {concept} {description} {total}
         </p>
       </Container>
     </Paper>
