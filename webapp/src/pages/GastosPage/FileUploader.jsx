7961f196c09f39ab31b0eafded5a339300ea6e2f
Omar Mendoza <74740411+wizeomaryahir@users.noreply.github.com>
2020-11-29 04:24:34 -0600

    ADD: Styling modal expenses and functionality with multiple observations, and form save to commit invoices

--- webapp/src/pages/GastosPage/FileUploader.jsx
+++ webapp/src/pages/GastosPage/FileUploader.jsx
@@ -29,24 +29,38 @@ const FileUploader = () => {
   const [description, setDescription] = useState('');
   const [total, setTotal] = useState('');
   const [openModal, setOpenModal] = React.useState(false);
+  const [status, setStatus] = useState('');
+  const [observations, setObservations] = useState([]);
 
   const api = new ServiceAPI();
 
-  async function uploadFile(file_upload) {
-    const data = new FormData();
+  async function uploadFile(event) {
+    let file_upload = event.target.files[0];
     let userId = await getCurrentLoggedUserInfo();
+    const data = new FormData();    
     data.append('xml', file_upload);
     data.append('uid', userId.googleId);
+    setOpenModal(false);
+    setObservations([]);
 
     api
       .createRequest()
       .post('invoices/upload', data, {})
       .then((res) => {
         setFileUUID(res.data.uuid);
+        setStatus(res.data.analysisResult.status);
+        setObservations(res.data.analysisResult.observations);
         setConcept(res.data.concept);
         setDescription(res.data.description);
         setTotal(res.data.total);
         setOpenModal(true);
+        event.target.value = null;
+      })
+      .catch((err) => {
+        setStatus('error');
+        setDescription(String(err));
+        setOpenModal(true);
+        event.target.value = null;
       });
   }
 
@@ -59,6 +73,8 @@ const FileUploader = () => {
           description={description}
           total={total}
           openModal={openModal}
+          status={status}
+          observations={observations}
         ></ModalGastos>
       }
       <Container>
@@ -78,7 +94,7 @@ const FileUploader = () => {
               <input
                 type="file"
                 hidden
-                onChange={(event) => uploadFile(event.target.files[0])}
+                onChange={(event) => uploadFile(event)}
               />
             </Button>
           </Grid>
